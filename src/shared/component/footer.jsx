e3b5ad8dd87ffc8b7591bbf1f460f57dc5ce2eb5
Celina Khalife <celina.khalife@postlight.com>
2018-05-25 14:54:42 +0300

    feat: initialize project

---
+++ src/shared/component/footer.jsx
@@ -0,0 +1,14 @@
+// @flow
+
+import React from 'react'
+import { APP_NAME } from '../config'
+
+const Footer = () =>
+  (<div className="container mt-5">
+    <hr />
+    <footer>
+      <p>© {APP_NAME} 2018</p>
+    </footer>
+  </div>)
+
+export default Footer
